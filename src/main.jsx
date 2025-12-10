efe5868ff3e8d746ecc3c2a3d5a0b910943e83e6
impacgo <adityaramipg123@gmail.com>
2025-12-10 09:44:41 +0530

    Update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+import { AuthProvider } from "./context/AuthContext";  // Add this
 import { UserProvider } from "./context/UserContext";
 import { LoadingProvider } from "./context/LoadingContext";
 import { ToastProvider } from "./context/ToastContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <LoadingProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </LoadingProvider>
-    </UserProvider>
+    <AuthProvider>  {/* Add AuthProvider here */}
+      <UserProvider>
+        <LoadingProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </LoadingProvider>
+      </UserProvider>
+    </AuthProvider>  {/* Close AuthProvider */}
   </React.StrictMode>
 );
