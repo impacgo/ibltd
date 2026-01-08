248b2b8526ccc90b3a5920bd1e4e089ace0d8459
impacgo <adityaramipg123@gmail.com>
2026-01-08 17:20:53 +0530

    Add files via upload

--- src/components/ServicePricing.jsx
+++ src/components/ServicePricing.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo, useCallback } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import "./ServicePricing.css"; // Changed from "./servicePricing.css"
+import "./ServicePricing.css";
 
 const API_BASE = "https://api.ironingboy.com";
 
