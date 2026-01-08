c1270ee05850b22e4131b3840e7121f0f2740cdf
impacgo <adityaramipg123@gmail.com>
2026-01-08 17:21:52 +0530

    Import ServicePricing.css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import "./components/ServicePricing.css";
+
 
 import { AuthProvider } from "./context/AuthContext";  // Add this
 import { UserProvider } from "./context/UserContext";
